82bbb6efeab122c53fe688fcbf05d55b98968fe0
Yutaka Tajika <y.tajika@wall.ac>
2015-09-11 06:27:39 +0000

    delete dotfilesLink.sh

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/vim/bundle ~/.vim
-ln -sf ~/dotfiles/vim/colors ~/.vim
-ln -sf ~/dotfiles/vim/ftdetect ~/.vim
-ln -sf ~/dotfiles/vim/indent ~/.vim
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/git/.gitconfig ~/
