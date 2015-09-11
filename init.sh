f4aa4d0b18752d1fd3a80347418c9f65a8a0dbee
Yutaka Tajika <y.tajika@wall.ac>
2015-09-11 08:28:48 +0000

    modified: init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ fi
 
 if [ ! -e ~/.vim/colors/ ]; then
 	mkdir ~/.vim/colors
-	cp -r ~/dotfiles/vim/colors ~/.vim/colors
+	cp -r ~/dotfiles/vim/colors ~/.vim
 fi
 
 ln -sf ~/dotfiles/vim/ftdetect ~/.vim
