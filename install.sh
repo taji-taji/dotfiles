9d725bdbc7d613cd3eef0c0737ece9129d211390
Yutaka Tajika <y.tajika529@gmail.com>
2015-11-02 17:30:28 +0900

    modified: submodules

--- install.sh
+++ install.sh
@@ -7,22 +7,6 @@ if [ ! -e ~/.vim/bundle/neobundle.vim/ ]; then
 	git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
 
-if [ ! -e ./.fzf ]; then
-	mkdir ./.fzf
-fi
-
-if [ ! -e ./.fzf/fzf ]; then
-	git clone https://github.com/junegunn/fzf.git ./.fzf
-fi
-
-if [ ! -e ./enhancd ]; then
-	mkdir ./enhancd
-fi
-
-if [ ! -e ./enhancd/enhancd.sh ]; then
-	git clone http://github.com/b4b4r07/enhancd.git ./enhancd
-fi
-
 if [ -e ~/.vim/colors ]; then
 	mv ~/.vim/colors ~/.vim/colors.bak
 fi
